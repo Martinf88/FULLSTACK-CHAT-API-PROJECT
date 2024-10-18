065c07c0e35e0b0ff0b40d8e6ae67f45544a8913
Martin Fahlström <makrill88@gmail.com>
2024-10-18 11:38:31 +0200

    LoginPage jsx and styling

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+
+function LoginPage() {
+	const [username, setUsername] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [isLoggedIn, SetIsLoggedIn] = useState<Boolean>(false);
+	
+	return (
+		<div className="login-page">
+				<h1 className="login-page__title">CHAPPY</h1>
+			<form action="" className="form container">
+				<h2 className="form-group">Welcome</h2>
+				<div className="form-group">
+				 <label htmlFor="username">Username:</label>
+				 <input
+				   type="text"
+				   id="username"
+				   value={username}
+				   onChange={(e) => setUsername(e.target.value)}
+				   required
+				 />
+			   </div>
+			   <div className="form-group">
+				 <label htmlFor="password">Password:</label>
+				 <input
+				   type="password"
+				   id="password"
+				   value={password}
+				   onChange={(e) => setPassword(e.target.value)}
+				   required
+				 />
+			   </div>
+			   <button type="submit" className="btn form-group">
+				 Log in
+			   </button>
+			   <a  href="">Continue as guest</a>
+			</form>
+		</div>
+	)
+}
+
+export default LoginPage
