5aab5cd91a3e045a2033cf8f288dcbd4eb7e16db
Martin Fahlström <makrill88@gmail.com>
2024-10-22 15:39:53 +0200

    redering channels in different lists based on isLocked

--- src/pages/ChannelsPage.tsx
+++ src/pages/ChannelsPage.tsx
@@ -53,6 +53,9 @@ function ChannelsPage() {
 	if (error) {
 		return <div>Error fetching channels: {error}</div>
 	}
+
+	const openChannels = channels.filter(channel => !channel.isLocked);
+	const lockedChannels = channels.filter(channel => channel.isLocked);
 	
 	return (
 		<section className="chat-rooms-section">
@@ -66,22 +69,28 @@ function ChannelsPage() {
 				</button>
 			</div>
 			<div className="channels-section">
-				<h2 className="channels__title">Channels</h2>
+				<h2 className="channels__title">Open Channels</h2>
 				<ul className="channels__list">
-					{channels.map(channel => (
+					{openChannels.map(channel => (
                     <li key={channel._id}>
                         <span>{channel.name}</span>
                         <span>
-							{channel.isLocked ? 
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-								</svg>
-							: 
 								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon">
 								<path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
 								</svg>
-							}
-
+						</span>
+                    </li>
+                ))}	
+				</ul>
+				<h2 className="channels__title locked__title">Locked Channels</h2>
+				<ul className="channels__list">
+					{lockedChannels.map(channel => (
+                    <li key={channel._id}>
+                        <span>{channel.name}</span>
+                        <span>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+							</svg>
 						</span>
                     </li>
                 ))}	
