80e8dd52c46b961159dc072efd4b3aba8949461c
Martin Fahlström <makrill88@gmail.com>
2024-10-22 14:43:27 +0200

    styling for the channels list

--- src/pages/ChannelsPage.tsx
+++ src/pages/ChannelsPage.tsx
@@ -65,19 +65,19 @@ function ChannelsPage() {
 
 				</button>
 			</div>
-			<div className="chat-rooms-section__channels">
+			<div className="channels-section">
 				<h2 className="channels__title">Channels</h2>
-				<ul>
+				<ul className="channels__list">
 					{channels.map(channel => (
                     <li key={channel._id}>
                         <span>{channel.name}</span>
                         <span>
 							{channel.isLocked ? 
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon lock">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon">
 								<path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
 								</svg>
 							: 
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon lock">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon">
 								<path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
 								</svg>
 							}
