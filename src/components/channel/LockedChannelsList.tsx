b393f50bee173536f907241e2463625c99dcb75b
Martin Fahlström <makrill88@gmail.com>
2024-11-07 08:58:04 +0100

    bug fixes

--- src/components/channel/LockedChannelsList.tsx
+++ src/components/channel/LockedChannelsList.tsx
@@ -1,28 +1,61 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Channel } from "../../models/channelModel";
+import { useAuthStore } from "../../store/authStore";
+import LogInModal from "../LogInModal";
 
 interface LockedChannelsListProps {
-	lockedChannels: Channel[];
+  lockedChannels: Channel[];
 }
 
-function LockedChannelsList({lockedChannels}:LockedChannelsListProps) {
-	
+function LockedChannelsList({ lockedChannels }: LockedChannelsListProps) {
+  const navigate = useNavigate();
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const setIsModalOpen = useAuthStore((state) => state.setIsModalOpen);
 
-	return (
-		<div className="channels__list">
-					{lockedChannels.map(channel => (
-                    <Link className="channel-link" to={`/chatroom/${channel._id}`} key={channel._id}>
-                        <span>{channel.name}</span>
-                        <span>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="xs-icon">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-							</svg>
-						</span>
-                    </Link>
-                ))}	
-				</div>
-	)
+  const handleClick = (e: React.MouseEvent, channelId: string) => {
+    e.preventDefault();
+    if (!isLoggedIn) {
+      setIsModalOpen(true);
+    } else {
+      navigate(`/chatroom/${channelId}`);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="channels__list">
+      {lockedChannels.map((channel) => (
+        <Link
+          className="channel-link"
+          to={`/chatroom/${channel._id}`}
+          key={channel._id}
+          onClick={(e) => handleClick(e, channel._id)}
+        >
+          <span>{channel.name}</span>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="xs-icon"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+              />
+            </svg>
+          </span>
+        </Link>
+      ))}
+      <LogInModal onClose={closeModal} message="Please log in to add a DM" />
+    </div>
+  );
 }
 
-
-export default LockedChannelsList
+export default LockedChannelsList;
--- src/pages/ChatRoomPage.tsx
+++ src/pages/ChatRoomPage.tsx
@@ -1,61 +1,107 @@
-import { useParams } from "react-router-dom"
-import { useChatStore } from "../store/chatStore"
-import { useAuthStore } from "../store/authStore"
+import { Link, useParams } from "react-router-dom";
+import { useChatStore } from "../store/chatStore";
+import { useAuthStore } from "../store/authStore";
 import useUsers from "../hooks/useUsers";
 import useChannels from "../hooks/useChannels";
-import useMessages from "../hooks/useMessages"
+import useMessages from "../hooks/useMessages";
 import ChatRoomNavBar from "../components/chatroom/ChatRoomNavBar";
 import ChatRoomMessages from "../components/chatroom/ChatRoomMessages";
 import SendMessage from "../components/chatroom/ChatRoomSendMessage";
 
 function ChatRoomPage() {
-	const { channelId } = useParams<{ channelId: string }>()
-	const isLoggedIn = useAuthStore(state => state.isLoggedIn)
-	const channels = useChatStore(state => state.channels)
-	const users = useAuthStore(state => state.users)
-	const username = useAuthStore((state) => state.username);
+  const { channelId } = useParams<{ channelId: string }>();
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const channels = useChatStore((state) => state.channels);
+  const users = useAuthStore((state) => state.users);
+  const username = useAuthStore((state) => state.username);
 
-	if (!channelId) {
-        return <p>Error: Channel ID is required.</p>;
-    }
+  if (!channelId) {
+    return <p>Error: Channel ID is required.</p>;
+  }
 
-	const { error: messageError, messages, isLoading: isMessagesLoading, fetchMessages, } = useMessages(channelId); 
-	const { error: userError, isLoading: isUserLoading } = useUsers();
-	const { error: channelError, isLoading: isChannelLoading } = useChannels();
-	
-	const channel = channels.find((ch) => ch._id === channelId)
-	const currentUser = users.find((user) => user.username === username);
+  const {
+    error: messageError,
+    messages,
+    isLoading: isMessagesLoading,
+    fetchMessages,
+  } = useMessages(channelId);
+  const { error: userError, isLoading: isUserLoading } = useUsers();
+  const { error: channelError, isLoading: isChannelLoading } = useChannels();
 
-	if (isMessagesLoading || isChannelLoading || isUserLoading) {
-		return <p>Loading data...</p>;
-	}
+  const channel = channels.find((ch) => ch._id === channelId);
+  const currentUser = users.find((user) => user.username === username);
 
-	if (channelError || userError) {
-		return <p>Error: {channelError || userError}</p>;
-	}
+  if (isMessagesLoading || isChannelLoading || isUserLoading) {
+    return <p>Loading data...</p>;
+  }
 
-	if (!channel) {
-		return (
-			<div className="chat-room">
-				<p>Channel not found.</p>
-			</div>
-		);
-	}
+  if (channelError || userError) {
+    return (
+      <div>
+        <Link to={"/channels"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="small-icon"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </Link>
+        <p>Error: {channelError || userError}</p>;
+      </div>
+    );
+  }
 
-	
-	const senderId = currentUser ? currentUser._id : null;
+  if (!channel) {
+    return (
+      <div className="chat-room">
+        <Link to={"/channels"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="small-icon"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </Link>
+        <p>Channel not found.</p>
+      </div>
+    );
+  }
 
-	const shouldShowInput = !channel.isLocked || (channel.isLocked && isLoggedIn && senderId);
+  const senderId = currentUser ? currentUser._id : null;
 
-	return (
-		<div className="chat-room container">
-			<ChatRoomNavBar channelName={channel.name} />
-			<ChatRoomMessages messages={messages} messageError={messageError} />
-			{shouldShowInput && (
-			<SendMessage channelId={channelId} senderId={senderId} isLocked={channel.isLocked} refreshMessages={fetchMessages} />
-			)}
-		</div>
-	  );
+  const shouldShowInput =
+    !channel.isLocked || (channel.isLocked && isLoggedIn && senderId);
+
+  return (
+    <div className="chat-room container">
+      <ChatRoomNavBar channelName={channel.name} />
+      <ChatRoomMessages messages={messages} messageError={messageError} />
+      {shouldShowInput && (
+        <SendMessage
+          channelId={channelId}
+          senderId={senderId}
+          isLocked={channel.isLocked}
+          refreshMessages={fetchMessages}
+        />
+      )}
+    </div>
+  );
 }
 
-export default ChatRoomPage
+export default ChatRoomPage;
