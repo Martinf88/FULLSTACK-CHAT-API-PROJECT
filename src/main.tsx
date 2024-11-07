3791a5dd7d2389ed9e33f82e18aba7992b6699c2
Martin Fahlström <makrill88@gmail.com>
2024-11-07 10:27:14 +0100

    fetching users when entering as guest. and emptying local storage correctly when guest clicks button in navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './routes/root.tsx'
-import ErrorPage from './error-page.tsx'
-import LoginPage from './pages/LoginPage.tsx'
-import ChannelsPage from './pages/ChannelsPage.tsx'
-import ChatRoomPage from './pages/ChatRoomPage.tsx'
-import DMChatPage from './pages/DMChatPage.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./root.tsx";
+import ErrorPage from "./error-page.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import ChannelsPage from "./pages/ChannelsPage.tsx";
+import ChatRoomPage from "./pages/ChatRoomPage.tsx";
+import DMChatPage from "./pages/DMChatPage.tsx";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Root/>,
-		errorElement: <ErrorPage/>,
-		children: [
-			{
-				path: '/',
-				element: <LoginPage/>
-			},
-			{
-				path: "/channels",
-				element: <ChannelsPage/>
-			},
-			{
-				path: '/chatroom/:channelId',
-				element: <ChatRoomPage/>
-			},
-			{
-				path: '/dm',
-				element: <DMChatPage/>
-			},
-		]
-	},
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <LoginPage />,
+      },
+      {
+        path: "/channels",
+        element: <ChannelsPage />,
+      },
+      {
+        path: "/chatroom/:channelId",
+        element: <ChatRoomPage />,
+      },
+      {
+        path: "/dm",
+        element: <DMChatPage />,
+      },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,68 +1,103 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useLogin from "../hooks/useLogin";
+import useUsers from "../hooks/useUsers";
 
 function LoginPage() {
-	const [inputUsername, setInputUsername] = useState<string>('');
-	const [password, setPassword] = useState<string>('');
-	const [error, setError] = useState<string | null>(null);
-	const [isLoading, setIsLoading] = useState<boolean>(false)
-	const { loginUser } = useLogin()
-	
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		setError(null);
-		setIsLoading(true)
-	
-		try {
-			await loginUser({ username: inputUsername, password });
-		} catch (err) {
-			setError(err instanceof Error ? err.message : 'An unexpected error occurred.');
-		} finally {
-			setIsLoading(false)
-		}
-	}
-	
-	return (
-		<div className="login-page container">
-			<div className="title-wrapper">
-				<h1 className="form-group title">ELDEN 	CHAPPY</h1>
-				<h2 className="tagline form-group">Connect with friends and enemeis!</h2>
-			</div>
-			<form onSubmit={handleSubmit} className="form">
-				<h2 className="form-group form__subtitle">Enter<br />The Lands Between!</h2>
-				<div className="form-group form-username">
-				 <label htmlFor="username">Username</label>
-				 <input
-				   type="text"
-				   id="username"
-				   value={inputUsername}
-				   onChange={(e) => setInputUsername(e.target.value)}
-				   required
-				 />
-			   </div>
-			   <div className="form-group form-password">
-				 <label htmlFor="password">Password</label>
-				 <input
-				   type="password"
-				   id="password"
-				   value={password}
-				   onChange={(e) => setPassword(e.target.value)}
-				   required
-				 />
-			   </div>
-			   <button 
-				type="submit" 
-				className="btn form-group login-btn"
-				disabled={isLoading}
-				>
-				 {isLoading ? <span>Loading...</span> : <span>Log in</span>}
-			   </button>
-			   {error && <p className="error-message"> {error} </p>}
-			   <Link className="guest-link"  to={'/channels'}>Continue as guest</Link>
-			</form>
-		</div>
-	)
+  const [inputUsername, setInputUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { loginUser } = useLogin();
+  const { fetchUsers } = useUsers();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true);
+
+    try {
+      await loginUser({ username: inputUsername, password });
+    } catch (err) {
+      setError(
+        err instanceof Error ? err.message : "An unexpected error occurred."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleGuestClick = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true);
+    try {
+      await fetchUsers();
+    } catch (err) {
+      setError(
+        err instanceof Error ? err.message : "An unexpected error occurred."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+
+    navigate("channels");
+  };
+
+  return (
+    <div className="login-page container">
+      <div className="title-wrapper">
+        <h1 className="form-group title">ELDEN CHAPPY</h1>
+        <h2 className="tagline form-group">
+          Connect with friends and enemeis!
+        </h2>
+      </div>
+      <form onSubmit={handleSubmit} className="form">
+        <h2 className="form-group form__subtitle">
+          Enter
+          <br />
+          The Lands Between!
+        </h2>
+        <div className="form-group form-username">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={inputUsername}
+            onChange={(e) => setInputUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group form-password">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn form-group login-btn"
+          disabled={isLoading}
+        >
+          {isLoading ? <span>Loading...</span> : <span>Log in</span>}
+        </button>
+        {error && <p className="error-message"> {error} </p>}
+        <Link
+          className="guest-link"
+          to={"/channels"}
+          onClick={(e) => handleGuestClick(e)}
+        >
+          Continue as guest
+        </Link>
+      </form>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
